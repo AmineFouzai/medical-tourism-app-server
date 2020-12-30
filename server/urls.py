15ef26b35b65633b8e6a2dfb162ea505c6338d86
MedAmineFouzai <54685665+MedAmineFouzai@users.noreply.github.com>
2020-12-30 01:01:59 +0100

    Update urls.py

--- server/urls.py
+++ server/urls.py
@@ -4,13 +4,10 @@ from graphene_django.views import GraphQLView
 from graphql_playground.views import GraphQLPlaygroundView 
 from django.views.decorators.csrf import csrf_exempt
 
-
-
 urlpatterns = [
     path('jet/', include('jet.urls', 'jet')),
     path('admin/', admin.site.urls),
     path('',include('app.urls')),
     path("graphql/", csrf_exempt(GraphQLView.as_view(graphiql=True))),
     path('playground/', GraphQLPlaygroundView.as_view(endpoint="http://127.0.0.1:8000/graphql/")),
-  
 ]
